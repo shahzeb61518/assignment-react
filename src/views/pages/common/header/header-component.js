dc3092bbdae5fcdc5b6436f497b589cbeb00b113
shazab <shahzeb61518@gmail.com>
2021-04-16 18:46:47 +0500

    minor changes

--- src/views/pages/common/header/header-component.js
+++ src/views/pages/common/header/header-component.js
@@ -3,7 +3,6 @@ import { withRouter } from "react-router";
 import { ROUTES } from "../../../../helpers/RoutePaths";
 
 function HeaderComponent(props) {
-  console.log("propsssssssssssss.", props);
   return (
     <div className="header">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
